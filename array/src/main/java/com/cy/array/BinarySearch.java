860e7fb1c683012392937b093dcf71ebfff488a1
Lil-Kr <cxqybox@163.com>
2023-08-23 23:13:12 +0800

    array algorithm

---
+++ array/src/main/java/com/cy/array/BinarySearch.java
@@ -0,0 +1,13 @@
+package com.cy.array;
+
+/**
+ * @Author: Lil-K
+ * @Date: 2023/8/23
+ * @Description: 二分查找法
+ */
+public class BinarySearch {
+
+
+
+
+}
---
+++ common/src/main/java/com/cy/algorithm/common/NumberUtil.java
@@ -0,0 +1,38 @@
+package com.cy.algorithm.common;
+
+/**
+ * @Author: Lil-K
+ * @Date: 2023/8/23
+ * @Description: 生成数字工具类
+ */
+public class NumberUtil {
+
+    private NumberUtil(){}
+
+    /**
+     * 生成指定范围内的整数数组
+     * @param n
+     * @param rangeL
+     * @param rangeR
+     * @return
+     */
+    public static Integer[] generateRandomArray(int n, int rangeL, int rangeR) {
+        assert n > 0 && rangeL <= rangeR;
+
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = (int)(Math.random() * (rangeR - rangeL + 1)) + rangeL;
+        return arr;
+    }
+
+
+    public static Integer[] generateOrderedArray(int n) {
+        assert n > 0;
+
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = i;
+        return arr;
+    }
+
+}
