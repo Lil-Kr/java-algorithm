7000dc52e3f9d01f4e9330a13564fd7a0a58054c
Lil-Kr <cxqybox@163.com>
2023-09-06 23:16:45 +0800

    sort

--- common/src/main/java/com/cy/algorithm/common/execution/Execution.java
+++ common/src/main/java/com/cy/algorithm/common/execution/Execution.java
@@ -36,7 +36,7 @@ public class Execution {
 
     public static boolean isSortedMaxToMin(Comparable[] arr) {
         for( int i = 0; i < arr.length - 1; i++)
-            if( arr[i].compareTo(arr[i+1]) > 0 )
+            if( arr[i].compareTo(arr[i+1]) < 0 )
                 return false;
         return true;
     }
--- sort/src/test/java/com/cy/algorithm/sort/heapsort/HeapSortTest.java
+++ sort/src/test/java/com/cy/algorithm/sort/heapsort/HeapSortTest.java
@@ -24,19 +24,19 @@ class HeapSortTest {
         Comparable[] arr = {3, 2, 6, 8, 1, 4, 5, 7};
         HeapSort.sort(arr);
         System.out.println(StringPrint.printArray(arr));
-        Assertions.assertTrue(Execution.isSortedMinToMax(arr));
+        Assertions.assertTrue(Execution.isSortedMaxToMin(arr));
     }
 
     @Test
     public void test_2() throws Exception {
-        int n = 1_000_000;
-//        int n = 100_000;
+//        int n = 1_000_000;
+        int n = 100_000;
         Comparable[] arr = NumberUtil.generateRandomArray(n, 0, n);
         Comparable[] arr1 = Arrays.copyOf(arr, arr.length);
 
         Execution.executor(HeapSort.class, "sort", arr);
         Execution.executor(QuickSort3Ways.class, "quicksort", arr1);
-        Assertions.assertTrue(Execution.isSortedMinToMax(arr));
+        Assertions.assertTrue(Execution.isSortedMaxToMin(arr));
     }
 
     @Test
@@ -44,7 +44,7 @@ class HeapSortTest {
         Comparable[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         HeapSort.sort(arr);
         System.out.println(StringPrint.printArray(arr));
-//        Assertions.assertTrue(Execution.isSortedMinToMax(arr));
+        Assertions.assertTrue(Execution.isSortedMaxToMin(arr));
     }
 
 }
